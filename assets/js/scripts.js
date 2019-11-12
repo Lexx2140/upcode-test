5a765c931fe470b7932740434e0f81685c300584
Alex Pavlyuk <opavlyuk@gmail.com>
2019-11-12 19:26:41 +0200

    Refactoring

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,21 @@
 // Offcanvas menu
 
 let offMenu = document.querySelector('.off-menu');
-let burger = document.querySelector('.burger');
+let burger = document.querySelector('#offMenuToggle');
 
 burger.addEventListener('click', e => {
 	offMenu.classList.toggle('visible');
 	burger.classList.toggle('open');
 });
 
+
+// Slider
+// $(function() {
+// 	$(".owl-carousel").owlCarousel({
+// 		loop:true,
+// 	    items: 1,
+// 	    margin: 0
+// 	    // nav: true,
+// 	    // dots: true
+// 	});
+// });
